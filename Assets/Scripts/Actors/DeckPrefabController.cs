f0109dd3732bb7aafefce8ca1853e3fc9f7ed46d
BiscuitPrime <henri.nomico@gmail.com>
2024-04-06 18:19:35 +0200

    Added working prefab deck that launches the first encounter

---
+++ Assets/Scripts/Actors/DeckPrefabController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script used by the PREFAB of the deck used by the player.
+/// Only serves to trigger the FIRST encounter upon being clicked on.
+/// </summary>
+[RequireComponent(typeof(Collider2D))]
+public class DeckPrefabController : MonoBehaviour
+{
+    private Collider2D _collider;
+    private void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+        _collider.enabled = true;
+    }
+
+    private void OnMouseDown()
+    {
+        _collider.enabled = false;
+        GameManager.Instance.StartGame();
+    }
+}
