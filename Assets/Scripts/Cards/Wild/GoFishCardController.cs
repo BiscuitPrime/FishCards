1981e1bede632a3182b4400b0c69474b80923938
BiscuitPrime <henri.nomico@gmail.com>
2024-04-08 22:18:40 +0200

    Added working GoFish card

---
+++ Assets/Scripts/Cards/Wild/GoFishCardController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script used by the Go Fish card that ends the play immediately
+/// </summary>
+[CreateAssetMenu(fileName = "GoFish", menuName = "Scriptable Objects/Cards/Wild Cards/Go Fish")]
+public class GoFishCardController : WildCardController
+{
+    public override void ActivateCardEffect(GameObject card)
+    {
+        PlayController.Instance.RequestEndOfPlay();
+        Destroy(card);
+    }
+}
--- Assets/Scripts/Cards/Wild/WildCardController.cs
+++ Assets/Scripts/Cards/Wild/WildCardController.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 /// <summary>
 /// Script that will handle the attack cards, that will have their main effect to be diverse and ACTIVATED IMMEDIATELY WHEN PLAYED
 /// </summary>
-[CreateAssetMenu(fileName = "WildCard", menuName = "Scriptable Objects/Cards/Wild Card")]
 public class WildCardController : CardController
 {
     public override void ActivateCardEffect(GameObject card)
     {
-        throw new System.NotImplementedException();
     }
 }
